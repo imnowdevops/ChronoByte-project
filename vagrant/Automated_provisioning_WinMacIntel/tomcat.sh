3ec1fd02054cb35aab90ed3d281864bc1dad0c19
imnowdevops <111400254+imnowdevops@users.noreply.github.com>
2024-07-13 17:48:00 +0530

    Update tomcat.sh

--- vagrant/Automated_provisioning_WinMacIntel/tomcat.sh
+++ vagrant/Automated_provisioning_WinMacIntel/tomcat.sh
@@ -66,7 +66,7 @@ mvn install
 systemctl stop tomcat
 sleep 20
 rm -rf /usr/local/tomcat/webapps/ROOT*
-cp target/vprofile-v2.war /usr/local/tomcat/webapps/ROOT.war
+cp target/vprofile-2.0.0.war /usr/local/tomcat/webapps/ROOT.war
 systemctl start tomcat
 sleep 20
 
